0d3b65d2db2bff4df3e46759413e2d23423ecfc1
k1LoW <k1lowxb@gmail.com>
2017-05-30 23:41:51 +0900

    bonsai

--- test/utsusemi.test.js
+++ test/utsusemi.test.js
@@ -1,14 +1,16 @@
+const describe = require('mocha').describe;
+const it = require('mocha').it;
 const assert = require('power-assert');
 const utsusemi = require('../src/lib/utsusemi');
 
-describe('utsusemi.path', () => {
-    it ('if path do not have querystring, utsusemiPath return original path', () => {
+describe('utsusemi.path()', () => {
+    it ('if path do not have querystring, return original path', () => {
         assert(utsusemi.path('/') === '/');
         assert(utsusemi.path('/work/') === '/work/');
         assert(utsusemi.path('/work') === '/work');
         assert(utsusemi.path('/img/logo.png') === '/img/logo.png');
     });
-    it ('if path have querystring, utsusemiPath return utsusemi path', () => {
+    it ('if path have querystring, return utsusemi path', () => {
         assert(utsusemi.path('/?page=3') === '/-utsusemi-7b2270616765223a2233227d');
         assert(utsusemi.path('/work/?page=3') === '/work/-utsusemi-7b2270616765223a2233227d');
         assert(utsusemi.path('/work?page=3') === '/work-utsusemi-7b2270616765223a2233227d');
@@ -16,14 +18,14 @@ describe('utsusemi.path', () => {
     });
 });
 
-describe('utsusemi.realPath', () => {
-    it ('if path do not have querystring, realPath return original path', () => {
+describe('utsusemi.realPath()', () => {
+    it ('if path do not have querystring, return original path', () => {
         assert(utsusemi.realPath('/') === '/');
         assert(utsusemi.realPath('/work/') === '/work/');
         assert(utsusemi.realPath('/work') === '/work');
         assert(utsusemi.realPath('/img/logo.png') === '/img/logo.png');
     });
-    it ('if path have `-utsusemi-` separator, realPath return real path', () => {
+    it ('if path have `-utsusemi-` separator, return real path', () => {
         assert(utsusemi.realPath('/-utsusemi-7b2270616765223a2233227d') === '/?page=3');
         assert(utsusemi.realPath('/work/-utsusemi-7b2270616765223a2233227d') === '/work/?page=3');
         assert(utsusemi.realPath('/work-utsusemi-7b2270616765223a2233227d') === '/work?page=3');
@@ -31,14 +33,14 @@ describe('utsusemi.realPath', () => {
     });
 });
 
-describe('utsusemi.bucketKey', () => {
-    it ('if path do not have querystring, bucketKey return S3 object key path', () => {
+describe('utsusemi.bucketKey()', () => {
+    it ('if path do not have querystring, return S3 object key path', () => {
         assert(utsusemi.bucketKey('/') === 'index.html');
         assert(utsusemi.bucketKey('/work/') === 'work/index.html');
         assert(utsusemi.bucketKey('/work') === 'work');
         assert(utsusemi.bucketKey('/img/logo.png') === 'img/logo.png');
     });
-    it ('if path have querystring, bucketKey return S3 object key path using utsusemiPath', () => {
+    it ('if path have querystring, return S3 object key path using utsusemiPath', () => {
         assert(utsusemi.bucketKey('/?page=3') === 'index-utsusemi-7b2270616765223a2233227d.html');
         assert(utsusemi.bucketKey('/work/?page=3') === 'work/index-utsusemi-7b2270616765223a2233227d.html');
         assert(utsusemi.bucketKey('/work?page=3') === 'work-utsusemi-7b2270616765223a2233227d');
