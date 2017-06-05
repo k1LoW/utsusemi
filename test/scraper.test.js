401a7daf45d3181b18191abe87930bc77fbb3fd6
k1LoW <k1lowxb@gmail.com>
2017-06-05 16:17:50 +0900

    Fix bugs
    - Catch CSS parse error and manual parse
    - Add start delay to wait queuing
    - Catch 304 request error
    - Catch 403, 404, 410 request error

---
+++ test/scraper.test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const fs = require('fs');
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+const assert = require('power-assert');
+const scraper = require('../src/lib/scraper');
+
+describe('scraper.scrapeCSS()', () => {
+    it ('Scrape valid CSS', () => {
+        const cssStr = fs.readFileSync(__dirname + '/fixtures/valid.css', 'utf8');
+        const path = '/path/to/';
+        const scraped = scraper.scrapeCSS(cssStr, path);
+        assert(scraped[1].toString() === [].toString());
+    });
+    it ('Scrape valid CSS with `url()`', () => {
+        const cssStr = fs.readFileSync(__dirname + '/fixtures/valid_with_url.css', 'utf8');
+        const path = '/path/to/';
+        const scraped = scraper.scrapeCSS(cssStr, path);
+        assert(scraped[0].match('/img/logo.png'));
+        assert(scraped[0].match('/path/img/title.png'));
+        assert(scraped[1].toString() === ['/img/logo.png', '/path/img/title.png'].toString());
+    });
+    it ('Scrape invalid CSS', () => {
+        const cssStr = fs.readFileSync(__dirname + '/fixtures/invalid.css', 'utf8');
+        const path = '/path/to/';
+        const scraped = scraper.scrapeCSS(cssStr, path);
+        assert(scraped[0] === cssStr);
+        assert(scraped[1].toString() === [].toString());
+    });
+    it ('Scrape invalid CSS with `url()`', () => {
+        const cssStr = fs.readFileSync(__dirname + '/fixtures/invalid_with_url.css', 'utf8');
+        const path = '/path/to/';
+        const scraped = scraper.scrapeCSS(cssStr, path);
+        assert(scraped[0] !== cssStr);
+        assert(scraped[0].match('/img/logo.png'));
+        assert(scraped[0].match('/path/img/title.png'));
+        assert(scraped[1].toString() === ['/img/logo.png', '/path/img/title.png'].toString());
+    });
+});
